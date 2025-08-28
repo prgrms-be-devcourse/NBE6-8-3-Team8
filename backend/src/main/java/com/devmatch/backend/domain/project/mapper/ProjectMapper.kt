63a92af6d3d9f5d8cc94b7a051c52b59d3147376
flffkd12 <flffkd12@gmail.com>
2025-08-28 11:10:17 +0900

    [BE] refactor: ProjectMapper Java -> Kotlin Migration

--- backend/src/main/java/com/devmatch/backend/domain/project/mapper/ProjectMapper.kt
+++ backend/src/main/java/com/devmatch/backend/domain/project/mapper/ProjectMapper.kt
@@ -1,24 +1,23 @@
-package com.devmatch.backend.domain.project.mapper;
+package com.devmatch.backend.domain.project.mapper
 
-import com.devmatch.backend.domain.project.dto.ProjectDetailResponse;
-import com.devmatch.backend.domain.project.entity.Project;
-import java.util.Arrays;
+import com.devmatch.backend.domain.project.dto.ProjectDetailResponse
+import com.devmatch.backend.domain.project.entity.Project
 
-public class ProjectMapper {
-
-  public static ProjectDetailResponse toProjectDetailResponse(Project project) {
-    return new ProjectDetailResponse(
-        project.getId(),
-        project.getTitle(),
-        project.getDescription(),
-        Arrays.stream(project.getTechStack().split(", ")).toList(),
-        project.getTeamSize(),
-        project.getCurrentTeamSize(),
-        project.getCreator().getNickName(),
-        project.getStatus().name(),
-        project.getContent(),
-        project.getDurationWeeks(),
-        project.getCreatedAt()
-    );
-  }
+object ProjectMapper {
+    
+    fun toProjectDetailResponse(project: Project): ProjectDetailResponse {
+        return ProjectDetailResponse(
+            project.id,
+            project.title,
+            project.description,
+            project.techStack.split(", "),
+            project.teamSize,
+            project.currentTeamSize,
+            project.creator.nickname,
+            project.status.name,
+            project.content,
+            project.durationWeeks,
+            project.createdAt
+        )
+    }
 }
