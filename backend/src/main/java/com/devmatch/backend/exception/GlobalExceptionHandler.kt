3668e084f0555d79e87082da22914e48fd0773ad
flffkd12 <flffkd12@gmail.com>
2025-08-28 13:45:28 +0900

    [BE] refactor: 전역 예외처리 핸들러 수정
    * @RestControllerAdvice 도입을 통해 코드 간결화

--- backend/src/main/java/com/devmatch/backend/exception/GlobalExceptionHandler.kt
+++ backend/src/main/java/com/devmatch/backend/exception/GlobalExceptionHandler.kt
@@ -2,37 +2,39 @@ package com.devmatch.backend.exception
 
 import com.devmatch.backend.global.ApiResponse
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
-import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@ControllerAdvice
+@RestControllerAdvice
 class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleValidationExceptions(ex: MethodArgumentNotValidException): ResponseEntity<ApiResponse<Void>> {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationExceptions(ex: MethodArgumentNotValidException): ApiResponse<Void> {
         val message = ex.bindingResult
             .allErrors
             .map { it as FieldError }
             .joinToString("\n") { error -> "${error.field}-${error.code}-${error.defaultMessage}" }
 
-        return ResponseEntity.badRequest().body(ApiResponse(message))
+        return ApiResponse(message)
     }
 
     @ExceptionHandler(HttpMessageNotReadableException::class)
-    fun handleHttpMessageNotReadable(ex: HttpMessageNotReadableException): ResponseEntity<ApiResponse<Void>> {
-        return ResponseEntity.badRequest()
-            .body(ApiResponse("요청 값이 올바르지 않습니다: " + ex.mostSpecificCause.message))
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleHttpMessageNotReadable(ex: HttpMessageNotReadableException): ApiResponse<Void> {
+        return ApiResponse("요청 값이 올바르지 않습니다: " + ex.mostSpecificCause.message)
     }
 
     @ExceptionHandler(IllegalArgumentException::class)
-    fun handleIllegalArgument(ex: IllegalArgumentException): ResponseEntity<ApiResponse<Void>> =
-        ResponseEntity.badRequest().body(ApiResponse(ex.message))
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleIllegalArgument(ex: IllegalArgumentException): ApiResponse<Void> =
+        ApiResponse(ex.message)
 
     @ExceptionHandler(NoSuchElementException::class)
-    fun handleNoSuchElement(ex: NoSuchElementException): ResponseEntity<ApiResponse<Void>> =
-        ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse(ex.message))
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleNoSuchElement(ex: NoSuchElementException): ApiResponse<Void> = ApiResponse(ex.message)
 }
