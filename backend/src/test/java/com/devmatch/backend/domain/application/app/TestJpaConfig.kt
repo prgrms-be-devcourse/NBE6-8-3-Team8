3c8bfa4f20013198ef10e22d4863c1e4e8f4dacd
SeokGeunHo <45616952+seok6555@users.noreply.github.com>
2025-09-01 11:16:08 +0900

    [BE] fix: ApplicationRepositoryTest 오류 수정 (#70)

---
+++ backend/src/test/java/com/devmatch/backend/domain/application/app/TestJpaConfig.kt
@@ -0,0 +1,9 @@
+package com.devmatch.backend.domain.application.app
+
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@TestConfiguration
+@EnableJpaAuditing
+class TestJpaConfig {
+}
