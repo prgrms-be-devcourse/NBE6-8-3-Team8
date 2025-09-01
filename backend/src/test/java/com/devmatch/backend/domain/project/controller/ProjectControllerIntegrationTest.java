edaa917eb3d0bb12da2c32b29f5b661b3b7ddd24
flffkd12 <flffkd12@gmail.com>
2025-09-01 09:33:15 +0900

    [BE] refactor: 테스트 중 Spring 환경 사용 시 test 환경 사용하도록 수정

--- backend/src/test/java/com/devmatch/backend/domain/project/controller/ProjectControllerIntegrationTest.java
+++ backend/src/test/java/com/devmatch/backend/domain/project/controller/ProjectControllerIntegrationTest.java
@@ -22,9 +22,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
